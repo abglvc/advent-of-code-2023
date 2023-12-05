76107ac8d6ba469beea1a900a03f10ee2c0fb03b
Adnan <abogilovic1@gmail.com>
2023-12-05 01:59:57 +0100

    day4 py solution

---
+++ day4/day4.py
@@ -0,0 +1,29 @@
+############ GOLD_AOC_STAR #############
+GOLD_AOC_STAR = True
+############ GOLD_AOC_STAR #############
+
+############## READ_INPUT ##############
+input = None
+with open("day4/input.txt", 'r') as file:
+    input = file.read()
+############## READ_INPUT ##############
+
+input = input.split('\n')
+instances = [1 for _ in range(len(input))]
+res = 0
+
+for i in range(len(input)):
+    win_nums, elf_nums = input[i].split(':')[-1].split('|')
+    win_nums = [int(num) for num in win_nums.strip().split()]
+    elf_nums = [int(num) for num in elf_nums.strip().split()]
+    matches = 0
+    for wn in win_nums:
+        matches += (1 if wn in elf_nums else 0)
+    for j in range(matches):
+        instances[i+j+1] += instances[i]
+    if not GOLD_AOC_STAR and matches != 0: 
+        res += 2**(matches-1)
+
+if GOLD_AOC_STAR:
+    res = sum(instances)
+print(res)
